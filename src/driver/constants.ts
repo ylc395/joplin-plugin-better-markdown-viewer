c7c1a8e2427aa70aab8d8cca4fbe3283785f83b0
ylc395 <cyl@cyl.moe>
2021-11-10 10:22:45 +0800

    fix: add namespace for script id

--- src/driver/constants.ts
+++ src/driver/constants.ts
@@ -1,2 +1,2 @@
 export const MARKDOWN_SCRIPT_ID = 'ylc395.betterMarkdownViewer.contentScriptMarkdownIt';
-export const CODE_MIRROR_SCRIPT_ID = 'contentScriptCodeMirror';
+export const CODE_MIRROR_SCRIPT_ID = 'ylc395.betterMarkdownViewer.contentScriptCodeMirror';
